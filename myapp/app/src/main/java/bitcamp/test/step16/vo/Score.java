9c6f9c7946cd358b154a58157b51e0033a81d12e
robie0821 <robie0821@gmail.com>
2023-06-09 17:44:22 +0900

    .

---
+++ myapp/app/src/main/java/bitcamp/test/step16/vo/Score.java
@@ -0,0 +1,35 @@
+package bitcamp.test.step16.vo;
+
+public class Score {
+  private String name;
+  public int kor;
+  public int eng;
+  public int math;
+  private int sum;
+  private float aver;
+
+  public Score(String name, int kor, int eng, int math) {
+    this.name = name;
+    this.kor = kor;
+    this.eng = eng;
+    this.math = math;
+    this.compute();
+  }
+
+  public void compute() {
+    this.sum = this.kor + this.eng + this.math;
+    this.aver = this.sum / 3f;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getSum() {
+    return this.sum;
+  }
+
+  public float getAver() {
+    return this.aver;
+  }
+}
