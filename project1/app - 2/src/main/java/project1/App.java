9c49afcfe267f68ce76d4c6051574ad38b83e3e9
robie0821 <robie0821@gmail.com>
2023-06-30 17:26:37 +0900

    .

--- project1/app - 2/src/main/java/project1/App.java
+++ project1/app - 2/src/main/java/project1/App.java
@@ -8,18 +8,21 @@ public class App {
     System.out.println("**대학교 학점 관리 시스템");
     System.out.println("----------------------------------");
 
-    int no;
-    String name = "홍길동";
-    String C
+    System.out.print("학번: ");
+    System.out.println(1001);
 
-
-    System.out.printf("학번: %d\n", 1001);
     System.out.printf("이름: %s\n", "홍길동");
+
     System.out.printf("C언어: %s\n", "A+");
+
     System.out.printf("C++: %s\n", "A+");
+
     System.out.printf("자바: %s\n", "A+");
+
     System.out.printf("리눅스: %s\n", "A+");
+
     System.out.printf("학점: %f\n", 4.5);
+
     System.out.printf("장학금: %b\n", true);
   }
 }
